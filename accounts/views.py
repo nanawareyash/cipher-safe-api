7085f8b35ebb7f74012ccd7efdbdb7e8aacfa271
Yash N. <nanawareyash@gmail.com>
2024-02-08 23:58:27 +0530

    Created User API View to list the registered users

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.parsers import JSONParser
+from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.settings import api_settings
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.viewsets import GenericViewSet, ViewSet
 
-# Create your views here.
+from .models import Users
+from .serializers import UserSerializer
+
+
+class UserViewSet(ViewSet, GenericViewSet):
+    queryset = Users.objects.all()
+    parser_classes = [JSONParser]
+    serializer_class = UserSerializer
+    pagination_class = api_settings.DEFAULT_PAGINATION_CLASS
+
+    def get_permissions(self):
+        if self.action == "list":
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+    def list(self, request):
+        page = self.paginate_queryset(self.queryset)
+
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.serializer_class(page, many=True)
+
+        if serializer:
+            return Response(serializer.data, status=HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
