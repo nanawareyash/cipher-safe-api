4e9ed54a3d5a7850e8c28fa442eef3636ddd8fbc
Yash N. <nanawareyash@gmail.com>
2024-02-08 23:49:15 +0530

    Created User Model and register it to admin

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Users(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=100)
+    email = models.EmailField(verbose_name="Email", max_length=30)
+    country_code = models.CharField(
+        verbose_name="Country Code", max_length=4, blank=True, null=True
+    )
+    contact_no = models.CharField(
+        verbose_name="Contact No.", max_length=10, blank=True, null=True
+    )
+    password = models.CharField(verbose_name="Password", max_length=255)
+    email_verified = models.BooleanField(verbose_name="Email Verified", default=False)
+    contact_no_verified = models.BooleanField(
+        verbose_name="Email Verified", default=False
+    )
+    created_at = models.DateTimeField(verbose_name="Created At", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Updated At", auto_now=True)
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name_plural = "Users"
